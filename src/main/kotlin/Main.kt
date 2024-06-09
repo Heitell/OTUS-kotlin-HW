972e467b10a73d6e54a5e7e0661ddf23db541211
elenakruzhechkina <jam23@yandex.ru>
2024-06-09 16:23:31 +0300

    OTUS-HW2-collections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,22 @@
-import kotlin.reflect.KFunction1
+
 
 fun main() {
-    val numbers = listOf(1, 2, 3, 4, 5)
-    val transformedNumbers = transformCollection(numbers, ::square)
-    println(transformedNumbers) // вернется: [1, 4, 9, 16, 25]
+    people.groupBy { it.age }.forEach { (t, u) -> println(u.size) }
+
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        .split(' ', '.', ',')
+        .filter { it.startsWith('a') }
+        .groupBy { it.length }
+        .also { println(it.entries) }
 }
 
-fun square(x: Int): Int {
-    return x * x
-}
+data class Person(val name: String, val age: Int)
+val people = listOf(
+    Person("Петя", 25),
+    Person("Вася", 30),
+    Person("Даша", 25),
+    Person("Женя", 30),
+    Person("Алексей", 20)
+)
 
-fun <T> transformCollection(input: List<T>, block: KFunction1<T, T>) : List<T> {
-    return input.map {
-        block(it)
-    }
-}
+
